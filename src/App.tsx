e0493440704b06d8ffe7a597f6f66517ed7e80e3
Evhen Nagornyak <dolkiv@gmail.com>
2023-07-13 21:23:00 +0300

    added base css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import './App.css';
 import {TextInput} from "./text-input/text-input";
+import {Password} from "./password";
+import {MenuButtonInteraction} from "./menu-button-interaction";
+
+enum PATH {
+  HOME = '/',
+  MENU_BUTTON_INTERACTION = '/menu-button-interaction'
+}
+
 
 function App() {
+  const [password, setPassword] = useState('')
+  const [showPassword, setShowPasswordShow] = useState(false)
+
+  const toggleShowPassword = useCallback(() => setShowPasswordShow(s => !s), [])
+
+  const path = window.location.pathname
+
   return (
-    <>
-      <TextInput label={'Name'}/>
-    </>
-  );
+    <div className={'app'}>
+      <div className="nav">
+        <a href="/">Home</a>
+        <a href="/menu-button-interaction">menu-button-interaction</a>
+      </div>
+      <main className="main">
+        {path === PATH.HOME &&
+          <>
+            <TextInput label={'Name'}/>
+            <Password value={password} showPassword={showPassword} onChange={setPassword}
+                      toggleShowPassword={toggleShowPassword}/>
+          </>}
+        {path === PATH.MENU_BUTTON_INTERACTION && <MenuButtonInteraction/>}
+      </main>
+    </div>
+  )
+    ;
 }
 
 export default App;
